8b0a3a636b4604e1880300c3369e77b24ef9fe5a
Skyler Berg <4156131+skylerberg@users.noreply.github.com>
2025-01-24 16:42:43 -0800

    Add polyfill for drag and drop on mobile

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+  import {polyfill} from "mobile-drag-drop";
+
   import Board from './lib/Board.svelte';
   import TileBag from './lib/TileBag.svelte';
+
+  polyfill({})
 </script>
 
 <h1>Tile Game</h1>
