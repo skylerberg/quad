bf1d4e7d8d03ec9e33d9813e40fbd3d586719433
Skyler Berg <4156131+skylerberg@users.noreply.github.com>
2025-01-24 16:18:20 -0800

    Add base URL for github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/tile-game/',
   plugins: [svelte({
     preprocess: true,
   })],
