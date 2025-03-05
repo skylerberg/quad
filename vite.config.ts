e2c71a4118cc636c0b40ee74d7d0ea955871eb3e
Skyler Berg <4156131+skylerberg@users.noreply.github.com>
2025-03-04 21:19:42 -0800

    Rename some references to "tile-game" to be "quad"

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/tile-game/',
+  base: '/quad/',
   plugins: [svelte({
     preprocess: true,
   })],
