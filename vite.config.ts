2ea9e51d44a624b5ff17ea18655a9038ca198dc0
Skyler Berg <4156131+skylerberg@users.noreply.github.com>
2025-04-14 16:45:40 -0700

    Remove base URL path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/quad/',
+  base: '/',
   plugins: [svelte({
     preprocess: true,
   })],
