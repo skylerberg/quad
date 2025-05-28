7fc3f28ec46797638cfd46b5232660ec3c33ac6c
Skyler Berg <4156131+skylerberg@users.noreply.github.com>
2025-05-28 00:46:15 -0700

    Add first batch of graded levels

--- src/lib/levels/casual.ts
+++ src/lib/levels/casual.ts
@@ -2,6 +2,32 @@ import { levelFromString } from '../level';
 
 const levels = [
   levelFromString('4rbb4344g1123rrr1311g433ggbgwrww', '01B313R323G220B2'),
+  levelFromString('1211ggbg224433324rww3423brrr4bbb', '02R403W212W431G4'),
+  levelFromString('g4gb14142432wwbb1131rbrrw233wbww', '00G102B330B433W2'),
+  levelFromString('233344413bggwgwg1412rr4rbggb4w4w', '00R212R132B233G4'),
+  levelFromString('brrrggbb3233r31213134442bbg4ggrg', '03R312W220B223W1'),
+  levelFromString('33311114bbbrgwwgwwwr2441rbbg4r4w', '02R210G130G331R1'),
+  levelFromString('wwwg3322b4bg4441bbbg4232wwggg333', '00G112R222G433R1'),
+  levelFromString('bwbb43444ggg11212244wbwbr3rw3313', '01W403R220W333G1'),
+  levelFromString('1144wrrwbggg4w4w43231113bwwr3bbw', '03B311R332G233B4'),
+  levelFromString('4441rbwb2322rw4ww333ggwwrwrr2243', '00B113G133R3'),
+  levelFromString('grbw33314141www4gbggbr33bwwb1314', '00B210R212R430G4'),
+  levelFromString('rgrr4gr43134bbgbrbgrww4331334141', '02G111W221W122B3'),
+  levelFromString('41143332rrwrgr3g4341w4b4bbbw1221', '01G312R422W223B2'),
+  levelFromString('3433wgwgw4wr4124wr33ggrg3222wrrw', '02B103B412R2'),
+  levelFromString('wr3wrbrw44223343g441rrww2224rggg', '00W421R223B430G1'),
+  levelFromString('3222bwwb3344ww312rwrr444brgg3114', '03W310B213W430B3'),
+  levelFromString('11234222rbbg3bg34324r4rrrwwww4gg', '00G303G412B131W4'),
+  levelFromString('3334gggrwrwb11114bgg4b4w4212wwbr', '02R410R311G433W1'),
+  levelFromString('4113gwbgbbb42424rrwrg4g411143r31', '01W302R211B213G2'),
+  levelFromString('22224w4b1114grgwgrgg4br4brbb3423', '01G403G110R232R4'),
+  levelFromString('g3rbbrbb11133432rr4rwwwb1221g444', '00B311B223G233W4'),
+  levelFromString('r4bb11124443wwbw114433323rrrrwwg', '02G411G212G323B2'),
+  levelFromString('g4122224g44gbgww1bbb32114433rrrg', '00G212G321W432R3'),
+  levelFromString('1113rrwrwwbbbw12rbr4wwgb44143333', '01R203G220B133B3'),
+  levelFromString('wwwb3334w314rrrbgg4rbbrr2224bb41', '03R412B430W432W3'),
+  levelFromString('rrgrggbb3323r432gbbbb4443223g4gr', '00G112W213W433W1'),
+  levelFromString('1grgrrbr34331141wbgg4wg43bww4124', '01B210R112G330G4'),
 ];
 
 export default levels;
